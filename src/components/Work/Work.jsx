3225b4032b7a0337d2400eaca2a53d49ab25cfd7
mariameid <mariameid210@gmail.com>
2025-07-30 00:41:55 +0200

    added show button functionality

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -7,14 +7,17 @@ import WorkImg3 from "../../assets/img-3-pR2ZEVY9.jpg";
 import WorkImg4 from "../../assets/img-4-tHccITd-.jpg";
 import WorkImg5 from "../../assets/img-5-lAucr__a.jpg";
 import WorkImg6 from "../../assets/img-6-DFW03AZ3.jpg";
-
-let showMore = () => {};
+import { useState } from "react";
 
 function Work() {
+  const [ShowExtraImages, setShowExtraImages] = useState(false);
+  const showMore = () => {
+    setShowExtraImages(true);
+  };
   return (
     <>
-      <div className="d-flex  flex-column align-items-center ">
-        <div className=" w-75  mt-4  playfair-font">
+      <div className="d-flex  justify-content-center align-items-center ">
+        <div className=" w-75  my-4  playfair-font">
           <p className="text-secondary mt-4 fs-6">MY WORK</p>
           <h5 className="fw-bold my-4 ">RECENT WORK</h5>
           <NavLink to="" className="text-decoration-none me-3">
@@ -189,13 +192,99 @@ function Work() {
                 <img src={WorkImg4} alt="" className={styles.workImg} />
               </div>
             </div>
+            {ShowExtraImages && (
+              <>
+                <div className="col-lg-6 col-sm-12 ">
+                  <div className={`${styles.workImgWrapper}  `}>
+                    <div
+                      className={`${styles.imgOverlay}   d-flex flex-column  justify-content-around `}
+                    >
+                      <div className="d-flex flex-column  mx-3 ">
+                        <Link to="#" className=" text-white fs-5 ">
+                          WORK 02
+                        </Link>
+                        <span className=" text-white mt-3">Animation</span>
+                      </div>
+                      <div className="icons d-flex align-items-center gap-3 mx-3  ">
+                        <Link className=" text-dark">
+                          <span className={`${styles.iconBox}`}>
+                            <i className="fa-solid fa-share-nodes  "></i>
+                          </span>
+                        </Link>
+                        <Link className="text-dark text-decoration-none">
+                          <div
+                            className={`${styles.iconBox} eyeicon d-flex  align-items-center`}
+                          >
+                            <i className="fa-solid fa-eye  me-1"></i>
+
+                            <span className=" text-dark">100</span>
+                          </div>
+                        </Link>
+                        <Link className="text-dark text-decoration-none">
+                          <div
+                            className={`${styles.iconBox} d-flex  align-items-center`}
+                          >
+                            <i className="fa-regular fa-heart text-dark me-1"></i>
+
+                            <span>49</span>
+                          </div>
+                        </Link>
+                      </div>
+                    </div>
+                    <img src={WorkImg5} alt="" className={styles.workImg} />
+                  </div>
+                </div>
+                <div className="col-lg-6 col-sm-12 ">
+                  <div className={`${styles.workImgWrapper}  `}>
+                    <div
+                      className={`${styles.imgOverlay}   d-flex flex-column  justify-content-around `}
+                    >
+                      <div className="d-flex flex-column  mx-3 ">
+                        <Link to="#" className=" text-white fs-5 ">
+                          WORK 02
+                        </Link>
+                        <span className=" text-white mt-3">Animation</span>
+                      </div>
+                      <div className="icons d-flex align-items-center gap-3 mx-3  ">
+                        <Link className=" text-dark">
+                          <span className={`${styles.iconBox}`}>
+                            <i className="fa-solid fa-share-nodes  "></i>
+                          </span>
+                        </Link>
+                        <Link className="text-dark text-decoration-none">
+                          <div
+                            className={`${styles.iconBox} eyeicon d-flex  align-items-center`}
+                          >
+                            <i className="fa-solid fa-eye  me-1"></i>
+
+                            <span className=" text-dark">100</span>
+                          </div>
+                        </Link>
+                        <Link className="text-dark text-decoration-none">
+                          <div
+                            className={`${styles.iconBox} d-flex  align-items-center`}
+                          >
+                            <i className="fa-regular fa-heart text-dark me-1"></i>
+
+                            <span>49</span>
+                          </div>
+                        </Link>
+                      </div>
+                    </div>
+                    <img src={WorkImg6} alt="" className={styles.workImg} />
+                  </div>
+                </div>
+              </>
+            )}
           </div>
-          <button
-            className="btn btn-primary text-white px-3 py02 my-5"
-            onClick={showMore}
-          >
-            Show More
-          </button>
+          {!ShowExtraImages && (
+            <button
+              className="btn btn-primary text-white px-3 py02 my-5"
+              onClick={showMore}
+            >
+              Show More
+            </button>
+          )}
         </div>
       </div>
     </>
