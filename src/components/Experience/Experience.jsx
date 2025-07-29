bc1de6f10a06104a2de81debc9807d295b7100c1
mariameid <mariameid210@gmail.com>
2025-07-29 03:32:16 +0200

    finished experience page

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,5 +1,69 @@
 function Experience() {
-  return <div>experience</div>;
+  return (
+    <>
+      <div className="experience-content d-flex flex-column   align-items-center">
+        <div className="w-75 mt-4  playfair-font">
+          <p className="text-secondary mt-3 ">EXPERIENCE</p>
+          <h5 className="fw-bold  mb-5 ">WORK EXPERIENCE</h5>
+          <div className="row g-5 ">
+            <div className="col-lg-12 d-flex flex-column gap-3 ">
+              <div className="d-flex align-items-start gap-3 ">
+                <div className="bg-info text-white rounded-circle p-2 m-4 ">
+                  <i className="fa-solid fa-pencil fs-5 "></i>
+                </div>
+                <div className="exp-card bg-light p-3 shadow ">
+                  <div className="header d-flex   my-3">
+                    <h5 className="">Full Stack Developer</h5>
+                    <span className="text-secondary mx-3">2017-2018</span>
+                  </div>
+                  <p className="text-secondary">
+                    Tolerably earnestly middleton extremely distrusts she boy
+                    now not. Add and offered prepare how cordial two promise.
+                    Greatly who affixed suppose but enquire compact prepare all
+                    put. Added forth chief trees but rooms think may.
+                  </p>
+                </div>
+              </div>
+              <div className="d-flex align-items-start gap-3 ">
+                <div className="bg-danger text-white rounded-circle p-2 m-4">
+                  <i className="fa-solid fa-pencil fs-5 "></i>
+                </div>
+                <div className="exp-card bg-light p-3 shadow ">
+                  <div className="header d-flex   my-3">
+                    <h5 className="">Front End Developer at Google Company</h5>
+                    <span className="text-secondary mx-3">2017-2018</span>
+                  </div>
+                  <p className="text-secondary">
+                    Even the all-powerful Pointing has no control about the
+                    blind texts it is an almost unorthographic life One day
+                    however a small line of blind text by the name of Lorem
+                    Ipsum decided to leave for the far World of Grammar.
+                  </p>
+                </div>
+              </div>
+              <div className="d-flex align-items-start gap-3 ">
+                <div className="bg-warning text-white rounded-circle p-2 m-4">
+                  <i className="fa-solid fa-pencil fs-5 "></i>
+                </div>
+                <div className="exp-card bg-light p-3 shadow ">
+                  <div className="header d-flex   my-3">
+                    <h5 className="">System Analyst</h5>
+                    <span className="text-secondary mx-3">2017-2018</span>
+                  </div>
+                  <p className="text-secondary">
+                    Even the all-powerful Pointing has no control about the
+                    blind texts it is an almost unorthographic life One day
+                    however a small line of blind text by the name of Lorem
+                    Ipsum decided to leave for the far World of Grammar.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Experience;
