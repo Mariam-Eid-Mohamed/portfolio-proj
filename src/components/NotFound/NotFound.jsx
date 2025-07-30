1a1ba6ff126f22d14196e53a226bf1902c1304cb
mariameid <mariameid210@gmail.com>
2025-07-30 10:56:51 +0200

    adjusted not found page

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 function NotFound() {
   return (
-    <div className="text-center mt-5">
-      <h1>404 - Page Not Found</h1>
-      <p>The page you're looking for doesn't exist.</p>
-      <Link to="/" className="btn btn-primary mt-3">
-        Go to Home
-      </Link>
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <div className="text-center ">
+        <h1>404 - Page Not Found</h1>
+        <p>The page you're looking for doesn't exist.</p>
+        <Link to="/" className="btn btn-primary mt-3">
+          Go to Home
+        </Link>
+      </div>
     </div>
   );
 }
